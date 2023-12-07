c366030289b99c2ecf71ed8ecf151c7026d3558e
John Miller <jbmiller10@gmail.com>
2023-12-06 20:02:19 -0500

    Add Recent Recordings Widget; List Items Class

--- app/MainWindow.py
+++ app/MainWindow.py
@@ -38,28 +38,45 @@ class RecentRecordingsWidget(QWidget):
         self.add_button.clicked.connect(self.add_recording)
         self.recordings_list.itemClicked.connect(self.recording_clicked)
 
-    def add_recording(self, file_name):
-        creation_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # todo Extract the date and time from the filename or file properties
-        duration = "00:00:00"  # Placeholder, extract real duration
+    def add_recording(self, full_file_path):
+        recording_item_widget = RecordingListItem(full_file_path)
 
-        item = QListWidgetItem(f"{file_name}\n{creation_date} {duration}")
-        item.setData(Qt.UserRole, {
-            'filename': file_name,
-            'date': creation_date,
-            'duration': duration
-        })
+        item = QListWidgetItem(self.recordings_list)
+        item.setSizeHint(recording_item_widget.sizeHint())
+
         self.recordings_list.addItem(item)
+        self.recordings_list.setItemWidget(item, recording_item_widget)
+
+        # Optionally store the metadata in the item's data
+        item.setData(Qt.UserRole, recording_item_widget.metadata)
+
+    def load_recordings(self):
+        recordings_dir = "Recordings"
+        if not os.path.exists(recordings_dir):
+            print("Recordings directory not found.")
+            return
+
+        supported_formats = ['.mp3', '.wav', '.ogg', '.flac']
+        file_list = []
+
+        try:
+            for filename in os.listdir(recordings_dir):
+                file_extension = os.path.splitext(filename)[1]
+                if file_extension.lower() in supported_formats:
+                    file_path = os.path.join(recordings_dir, filename)
+                    file_list.append(file_path)
+
+            # Now file_list contains full paths of all recordings
+            for file_path in file_list:
+                self.add_recording(file_path)
+
+        except Exception as e:
+            print("An error occurred while loading recordings:", e)
+            traceback.print_exc()
 
     def recording_clicked(self, item: QListWidgetItem):
         filename = item.text()
         self.recordingSelected.emit(filename)
-
-    def populate_recordings(self, recordings):
-        self.recordings_list.clear()
-        for recording in recordings:
-            item = QListWidgetItem(recording['filename'])
-            item.setData(Qt.UserRole, recording)  # Store the entire recording info in the item
-            self.recordings_list.addItem(item)
 
     def set_style(self):
         self.setStyleSheet("""
@@ -79,6 +96,80 @@ class RecentRecordingsWidget(QWidget):
             }
         """)
 
+class RecordingListItem(QWidget):
+    def __init__(self, full_file_path, *args, **kwargs):
+        super(RecordingListItem, self).__init__(*args, **kwargs)
+
+        # Extract the filename without the extension
+        filename = os.path.basename(full_file_path)
+        filename_no_ext = os.path.splitext(filename)[0]
+
+        # Extract the creation date and duration from the file metadata
+        creation_date = datetime.datetime.fromtimestamp(
+            os.path.getmtime(full_file_path)
+        ).strftime("%Y-%m-%d %H:%M:%S")
+        audio = AudioSegment.from_file(full_file_path)
+        duration = str(datetime.timedelta(milliseconds=len(audio))).split('.')[0]
+
+        self.name_editable = QLineEdit(filename_no_ext)
+        self.name_editable = EditableLineEdit(filename_no_ext)
+        self.name_editable.editingFinished.connect(self.finishEditing)
+        self.date_label = QLabel(creation_date)
+        self.duration_label = QLabel(duration)
+
+        self.name_editable.setStyleSheet(
+            "QLineEdit { color: grey; font-size: 16px; border: none; background: transparent;font-family: Roboto; }")
+        self.date_label.setStyleSheet("color: grey; font-size: 12px;")
+        self.duration_label.setStyleSheet("color: grey; font-size: 12px;")
+
+        layout = QHBoxLayout()
+        v_layout = QVBoxLayout()
+        v_layout.addWidget(self.name_editable)
+        v_layout.addWidget(self.date_label)
+        v_layout.addStretch()  # Pushes the labels to the top
+
+
+        layout.addLayout(v_layout, 3)
+
+        layout.addStretch(1)
+
+        layout.addWidget(self.duration_label, 1)  # The second argument is the stretch factor
+
+        self.setLayout(layout)
+
+        # Align the duration label to the right
+        self.duration_label.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+
+        # Store metadata for later use
+        self.metadata = {
+            'full_path': full_file_path,
+            'filename': filename_no_ext,
+            'date': creation_date,
+            'duration': duration
+        }
+
+    def mouseDoubleClickEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.name_editable.setReadOnly(False)  # Allow editing
+            self.name_editable.setFocus(Qt.MouseFocusReason)
+
+    def finishEditing(self):
+        new_name = self.name_editable.text()
+
+class EditableLineEdit(QLineEdit):
+    def __init__(self, *args, **kwargs):
+        super(EditableLineEdit, self).__init__(*args, **kwargs)
+        self.setReadOnly(True)  # Start as read-only
+
+    def mouseDoubleClickEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.setReadOnly(False)  # Allow editing
+            self.selectAll()  # Optionally select all text to make editing easier
+            QLineEdit.mouseDoubleClickEvent(self, event)  # Pass the event to the base class
+
+    def focusOutEvent(self, event):
+        self.setReadOnly(True)  # Make read-only again when focus is lost
+        QLineEdit.focusOutEvent(self, event)  # Pass the event to the base class
 
 class MainTranscriptionWidget(QWidget):
     transcriptionStarted = pyqtSignal()
@@ -150,8 +241,6 @@ class MainTranscriptionWidget(QWidget):
             }
         """)
 
-
-# ... [rest of your imports]
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -186,6 +275,9 @@ class MainWindow(QMainWindow):
         self.main_layout = QHBoxLayout(self.central_widget)
         self.main_layout.addWidget(splitter)
 
+        # Load existing recordings
+        self.recent_recordings_widget.load_recordings()
+
         # Connect the signals to their respective slots
         self.recent_recordings_widget.recordingSelected.connect(self.on_recording_selected)
         self.main_transcription_widget.transcriptionStarted.connect(self.on_transcription_started)
@@ -197,12 +289,7 @@ class MainWindow(QMainWindow):
         self.status_bar = QStatusBar()
         self.setStatusBar(self.status_bar)
 
-        # Sample data to populate the recordings list
-        sample_recordings = [
-            {'filename': 'File_1.wav', 'date': '2022-12-06', 'duration': '00:02:33'},
-            # ... add other recording data
-        ]
-        self.recent_recordings_widget.populate_recordings(sample_recordings)
+
 
     def on_recording_selected(self, filename):
         # TODO: Implement what happens when a recording is selected
