5fdb6fc200116afc2495155d1b400918688517cf
John Miller <jbmiller10@gmail.com>
2023-12-12 01:46:37 -0500

    add toggle switch

--- app/MainTranscriptionWidget.py
+++ app/MainTranscriptionWidget.py
@@ -11,7 +11,7 @@ from PyQt6.QtWidgets import (
     QWidget, QHBoxLayout, QLabel, QListWidget, QSizePolicy,
     QPushButton, QSpacerItem, QFileDialog, QMenu, QListWidgetItem, QMainWindow,QComboBox,QTextEdit, QSplitter,QStatusBar
 )
-
+from app.ToggleSwitch import ToggleSwitch
 from app.TextEditor import TextEditor
 
 
@@ -30,29 +30,42 @@ class MainTranscriptionWidget(QWidget):
         self.transcription_type_combo.addItems([
             'Journal Entry', 'Meeting Minutes', 'Interview Summary'
         ])
-        self.upper_transcription_toolbar = QHBoxLayout()
         self.play_button = QPushButton()
         self.play_button.setIcon(QIcon('icons/play.svg'))  # path to 'play' icon
         self.play_button.setFixedSize(50, 50)  # Adjust size as needed
-        self.upper_transcription_toolbar.addWidget(self.play_button)
         self.save_button = QPushButton()
         self.save_button.setIcon(QIcon('icons/save.svg'))  # path to 'save' icon
         self.save_button.setFixedSize(50, 50)  # Adjust size as needed
-        self.upper_transcription_toolbar.addWidget(self.save_button)
 
-        self.horizontal_spacer = QWidget()
+
+        #top toolbar
         self.top_toolbar = QHBoxLayout()
+
+        #spacer
+        self.horizontal_spacer = QWidget()
         self.horizontal_spacer.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+        self.top_toolbar.addWidget(self.horizontal_spacer)
+
+        #raw label
+        self.raw_transcript_label = QLabel('Raw Transcript')
+        self.top_toolbar.addWidget(self.raw_transcript_label)
+        #toggle switch
+        self.mode_switch = ToggleSwitch()
+        self.mode_switch.setValue(0)
+        self.top_toolbar.addWidget(self.mode_switch)
+        #raw label
+        self.gpt_processed_label = QLabel('Processed W/ GPT')
+        self.top_toolbar.addWidget(self.gpt_processed_label)
+
+
         self.settings_button = QPushButton()
         self.settings_button.setIcon(QIcon('icons/settings.svg'))  # path to 'settings' icon
         self.settings_button.setIconSize(QSize(25,25))
         self.settings_button.setFixedSize(30, 30)  # Adjust size as needed
-        self.upper_transcription_toolbar.addWidget(self.settings_button)
         self.transcript_text = TextEditor()
         self.top_toolbar.addWidget(self.horizontal_spacer)
         self.top_toolbar.addWidget(self.settings_button)
         self.layout.addLayout(self.top_toolbar)
-        self.upper_transcription_toolbar.addWidget(self.transcription_type_combo)
         self.layout.addWidget(self.transcript_text)
 
         self.play_button.clicked.connect(self.toggle_transcription)
---
+++ app/ToggleSwitch.py
@@ -0,0 +1,45 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QSlider, QVBoxLayout, QWidget
+from PyQt6.QtCore import Qt
+
+class ToggleSwitch(QSlider):
+    def __init__(self, parent=None):
+        super().__init__(Qt.Orientation.Horizontal, parent)
+        self.setFixedSize(100, 30)
+        self.setMinimum(0)
+        self.setMaximum(1)
+        self.setValue(0)
+        self.setStyleSheet("""
+            QSlider::groove:horizontal {
+                border: 1px solid #999999;
+                height: 20px;
+                width: 80px;
+                margin: 0px;
+                border-radius: 10px;
+                background: white;
+            }
+            QSlider::handle:horizontal {
+                background: lightgray;
+                border: 1px solid #5c5c5c;
+                width: 40px;
+                height: 20px;
+                border-radius: 10px; 
+                position: relative;
+            }
+            QSlider::handle:horizontal:hover {
+                background: #2d2d2d;
+            }
+            QSlider::sub-page:horizontal {
+                background: #c7c7c7;
+                border-radius: 10px;
+            }
+        """)
+        self.setTickPosition(QSlider.TickPosition.NoTicks)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            # Determine the value to set based on the mouse position
+            if event.position().x() < self.width() / 2:
+                self.setValue(0)
+            else:
+                self.setValue(1)
+            event.accept()
